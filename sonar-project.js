2a5ad54e4542885bdb38f4860f1d9285a1e163da
Pavithrakn <123339948+Pavithrakn14@users.noreply.github.com>
2023-07-12 23:14:36 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.39.47.215:9980/',
+  serverUrl: 'http://15.207.113.246:9000/:9980/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_b6d2f5ade9e0b2a3499dadbd00b8ea2bb53b9990',
+	    'sonar.login': 'squ_c20ddb8cbd9d80a43a502ed5db4e4c408de6a832',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'mourish',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
